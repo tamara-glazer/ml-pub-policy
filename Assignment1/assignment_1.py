6682d5ca99a5421f3f8476600e9a48ca3013ec3d
tglazer <tglazer@internationaldevelopmentgroup.com>
2019-04-07 13:49:07 -0500

    Moving onto task 2

--- Assignment1/assignment_1.py
+++ Assignment1/assignment_1.py
@@ -4,15 +4,17 @@ DESCRIPTION
 Author: Tammy Glazer
 '''
 
-import numby as np
+import numpy as np
 import pandas as pd
 from sodapy import Socrata
 import seaborn as sns
 import matplotlib.pyplot as plt
+import xlsxwriter
 
 URL = 'data.cityofchicago.org'
 ENDPOINT = '6zsd-86xi'
 TOKEN = 'YNQSgf5W1B65zdCeAX4nY9CXl'
+CSV = 'crimes_2017_to_2018'
 
 
 def load_data(url=URL, token=TOKEN, endpoint=ENDPOINT):
@@ -36,63 +38,118 @@ def load_data(url=URL, token=TOKEN, endpoint=ENDPOINT):
 
     return df
 
+def read_csv(input_file=CSV):
+    '''
+    INSERT DOCSTRINg
+    '''
+
+    csv = pd.read_csv(CSV)
+    return csv
+
 
 def create_groups(df, level_1, level_2):
+    '''
+    INSERT DOCSTRING
+    '''
 
-    crimes_by_type = df.groupby([level_1, level_2], as_index=False).agg\
-                                ({'case_number': 'count'}).pivot\
-                                (index=level_2, columns=level_1)\
-                                ['case_number'].fillna(0).astype(int)
+    groups = df.groupby([level_1, level_2], as_index=False).agg({'Case Number':
+                        'count'}).pivot(index=level_2, columns=level_1)\
+                        ['Case Number'].fillna(0).astype(int)
     return groups
 
 
-def summarize_crimes_by_type(df):
+def summarize_crimes_by_type(df, output_file):
+    '''
+    INSERT DOCSTRING
+    '''
 
-    crimes_by_type = create_groups(df, 'year', 'primary_type')
+    crimes_by_type = create_groups(df, 'Year', 'Primary Type')
     crimes_by_type.index.name = None
-    crimes_by_type['total'] = crimes_by_type['2017'] + crimes_by_type['2018']
-    crimes_by_type['percent_change'] = crimes_by_type.pct_change\
-                                       (axis='columns')['2018'].round(2)
+    crimes_by_type['Total'] = crimes_by_type[2017] + crimes_by_type[2018]
+    crimes_by_type['Percent Change'] = crimes_by_type.pct_change\
+                                       (axis='columns')[2018].round(2)
+    crimes_by_type.to_excel(output_file)
 
-
-    #return a table
 
 def create_months(df):
+    '''
+    INSERT DOCSTRING
+    '''
 
-    df['date'] = df.date.astype('datetime64[M]')
-    df['month'] = df.date.dt.to_period('M')
+    df['Date'] = df.Date.astype('datetime64[M]')
+    df['Month'] = df.Date.dt.to_period('M')
 
     return df
 
+
 def create_crime_heatmap(df):
+    '''
+    INSERT DOCSTRING
+    '''
 
     df_with_months = create_months(df)
-    crimes_by_month = create_groups(df_with_months, 'month', 'primary_type')
+    crimes_by_month = create_groups(df_with_months, 'Month', 'Primary Type')
     crimes_by_month.index.name = None
+    crimes_by_month.sortlevel(level=0, ascending=False, inplace=True)
     sns.set(font_scale=0.5)
     heatmap = sns.heatmap(crimes_by_month,
                           cmap='Blues',
                           annot=True,
-                          annot_kws={'size':8},
+                          annot_kws={'size': 5},
+                          fmt='g',
                           cbar=False,
                           linewidths=0.5,
                           mask=(crimes_by_month == 0))
     heatmap.set_title('Number of Crimes by Month, 2017-2018')
     plt.show(heatmap)
+    # should sort
 
-def summarize_crimes_by_month(df):
+def summarize_crimes_by_month(df, output_file):
+    '''
+    ADD DOCTSRING
+    '''
 
     df_with_months = create_months(df)
-    df_with_months.groupby(['month'], as_index=False).agg({'case_number':
+    df_with_months.groupby(['Month'], as_index=False).agg({'Case Number':
                                                           'count'})
+    df_with_months.to_excel(output_file)
 
-def summarize_crimes_by_community_area(df):
 
-    df.groupby(['community_area'], as_index=False).agg({'case_number':
-               'count', 'arrest': 'sum'})
-    #.sort_values(by=['case_number'], ascending=False)
-    #ensure that the arrests are calculating correctly
-    #more heatmaps by location?
+def summarize_crimes_by_community_area(df, output_file):
+    '''
+    ADD DOCSTRING
+    '''
+
+    df.groupby(['Community Area'], as_index=False).agg({'Case Number':
+               'count', 'Arrest': 'sum'}).astype(int).sort_values\
+               ('Community Area')
+    df.to_excel(output_file)
+
+
+def summarize_arrests_over_time(df):
+    '''
+    ADD DOCSTRING
+    '''
+
+    arrests_over_time = create_groups(df, 'Arrest', 'Date')
+    arrests_over_time.sort_index().cumsum().plot()
+    sns.set(font_scale=0.5)
+    arrests_over_time.set_title('Number of Reported Crimes by Arrest Status, 2017-2018')
+    plt.show()
+    #Is this working right?
+
+    # more heatmaps by location???
+    # format the tables and put in a document
+
+def provide_summary_stats(df):
+    pass
+
+def more_community_area(df):
+    pass
+
+
+
+
 
 
 
